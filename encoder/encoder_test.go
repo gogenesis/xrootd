458cc621eb3a6b6b8f02662f351ffb3262256adf
Mikhail Ivchenko <ematirov@gmail.com>
2018-03-10 18:30:28 +0400

    Add benchmarks for (un)marshaling

--- encoder/encoder_test.go
+++ encoder/encoder_test.go
@@ -2,8 +2,9 @@ package encoder
 
 import (
 	"bytes"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type request struct {
@@ -12,6 +13,29 @@ type request struct {
 	C uint16
 	D [2]byte
 	Y []byte
+}
+
+type benchmarkRequest struct {
+	X   int32
+	A   uint8
+	A1  uint8
+	A2  uint8
+	A3  uint8
+	A4  int32
+	A5  int32
+	A6  int32
+	A7  int32
+	A8  int32
+	A9  int32
+	A10 int32
+	A11 int32
+	A12 int32
+	A13 int32
+	A14 int32
+	A16 int32
+	C   uint16
+	D   [10]byte
+	Z   [10]byte
 }
 
 type undecodable struct {
@@ -59,4 +83,26 @@ func TestDecodeUndecodable(t *testing.T) {
 	err := UnmarshalFromReader(buffer, actual)
 
 	assert.Error(t, err)
+}
+
+func BenchmarkMarshal(b *testing.B) {
+	br := benchmarkRequest{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := Marshal(br); err != nil {
+			b.Error(err)
+		}
+	}
+}
+
+func BenchmarkUnMarshal(b *testing.B) {
+	br := benchmarkRequest{}
+	var buffer = &bytes.Buffer{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buffer.Write(make([]byte, 78))
+		if err := UnmarshalFromReader(buffer, &br); err != nil {
+			b.Error(err)
+		}
+	}
 }
